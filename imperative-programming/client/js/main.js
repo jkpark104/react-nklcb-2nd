ce9943ee8b97bd77dcbcda9347f0c4443988e0af
jkpark104 <jkrang104@gmail.com>
2021-11-25 16:13:36 +0900

    feat: Modularize files

---
+++ imperative-programming/client/js/main.js
@@ -0,0 +1,36 @@
+import { translator } from './lib/index.js';
+
+if ('jQuery' in window) {
+  var _window = window,
+      jQuery = _window.jQuery;
+
+  var translatorApp = function translatorApp($) {
+    var $translateButton = $('.button__translation');
+
+    var handleTranslation = function handleTranslation() {
+      translator.toggleLanguageMode();
+      updateRenderUI();
+    };
+
+    var updateRenderUI = function updateRenderUI() {
+      var _translator$getConten = translator.getContents(),
+          headline = _translator$getConten.headline,
+          description = _translator$getConten.description;
+
+      var $contents = $('.contents');
+      var $headline = $contents.find(':header');
+      var $description = $('p', $contents);
+      $headline.text(headline);
+      $description.text(description);
+      var label = translator.getLabel();
+      $translateButton.attr({
+        title: label,
+        'aria-label': label
+      });
+    };
+
+    $translateButton.on('click', handleTranslation);
+  };
+
+  jQuery(document).ready(translatorApp);
+}
--- imperative-programming/client/src/main.js
+++ imperative-programming/client/src/main.js
@@ -1,57 +1,53 @@
-const textFormatting = (text) => text.toString().replace(/\n\s+/g, '').trim();
-const throwError = (errorMessage) => { throw new Error(errorMessage) };
-
-const translator = {
-  en: {
-    headline: 'Imperative Programming',
-    description: textFormatting(`
-      In computer science, imperative programming is a programming paradigm 
-      that uses statements that change a program's state.
-    `),
-  },
-  ko: {
-    headline: '명령형 프로그래밍',
-    description: textFormatting(`
-      컴퓨터 과학에서 명령형 프로그래밍(命令型 프로그래밍, 영어: imperative programming)은 
-      선언형 프로그래밍과 반대되는 개념으로, 프로그래밍의 상태와 상태를 변경시키는 구문의 관점에서 
-      연산을 설명하는 프로그래밍 패러다임의 일종이다.
-    `),
-  },
-
-  _languageMode: 'en',
-  get currentMode() {
-    return this._languageMode;
-  },
-
-  isKorean() {
-    return this.currentMode === 'ko';
-  },
-  isEnglish() {
-    return this.currentMode === 'en';
-  },
-  toggleLanguageMode() {
-    this._languageMode = this.isKorean() ? 'en' : 'ko';
-    return this;
-  },
-  getContents(item) {
-    const contents = this[this.currentMode];
-    const contentItem = contents[item];
-    return !item
-      ? contents
-      : contentItem
-      ? contentItem
-      : throwError(`${item}은 translator 콘텐츠 아이템이 아닙니다.`);
-  },
-  getLabel() {
-    return `${this.isKorean() ? '한 → 영' : '영 → 한'} 번역`;
-  },
-};
+import { translator } from './lib/index.js';
 
 /* 명령형 프로그래밍 ---------------------------------------------------------------- */
 
 /* 미션 1. Vanilla(JavaScript) 버전으로 작성 */
 
 /* 미션 2. jQuery 버전으로 작성 */
+if ('jQuery' in window) {
+  const { jQuery } = window;
+  const translatorApp = $ => {
+    // 버튼 요소 노드 찾기
+    // jQuery 능력
+    // jQuery 팩토리 함수 $(selector)
+    const $translateButton = $('.button__translation');
+
+    const handleTranslation = () => {
+      // 모드를 토글
+      translator.toggleLanguageMode();
+
+      // UI 업데이트 렌더링
+      updateRenderUI();
+    };
+
+    const updateRenderUI = () => {
+      // translator의 현재 모드의 콘텐츠 가져오기
+      const { headline, description } = translator.getContents();
+
+      // 업데이트 할 문서의 요소를 찾아서 jQuery 인스턴스화 참조
+      const $contents = $('.contents');
+      const $headline = $contents.find(':header');
+      const $description = $('p', $contents);
+
+      // 텍스트 콘텐츠 업데이트
+      $headline.text(headline);
+      $description.text(description);
+
+      // 번역 버튼의 레이블 업데이트
+      let label = translator.getLabel();
+      $translateButton.attr({
+        title: label,
+        'aria-label': label,
+      });
+    };
+
+    // 찾은 버튼에 이벤트 연결 (jQuery 능력 활용 .on())
+    $translateButton.on('click', handleTranslation);
+  };
+
+  jQuery(document).ready(translatorApp);
+}
 
 /* 미션 3. translator 객체를 class 문법으로 작성 */
 
